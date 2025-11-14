259f678374f5e2b3aa3a9297a29095abfc61a513
adindaaftmh <adindaqori123@gmail.com>
2025-11-14 10:01:04 +0700

    tambah url baru

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -8,6 +8,7 @@ return [
 
     'allowed_origins' => [
         'https://web-event-new.vercel.app', // frontend kamu di Vercel
+        'https://dynotix.vercel.app', //url baruw
         'http://localhost:5173',
         'http://127.0.0.1:5173',
     ],
